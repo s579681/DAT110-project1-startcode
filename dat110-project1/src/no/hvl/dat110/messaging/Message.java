52805bc0b10a70ad4daa1a00af6d6ee1589c7c3a
Stian <shauge2206@gmail.com>
2020-02-04 22:02:32 +0100

    lagde encapsulate metode

--- dat110-project1/src/no/hvl/dat110/messaging/Message.java
+++ dat110-project1/src/no/hvl/dat110/messaging/Message.java
@@ -23,15 +23,22 @@ public class Message {
 	public byte[] encapsulate() {
 		
 		byte[] encoded = null;
-		
 		// TODO
 		// encapulate/encode the payload of this message in the
 		// encoded byte array according to message format
+		//type casting: byte –> short –> int –> long –> float –> double
+		encoded = new byte[128];
+		int sizeOfMsg = payload.length;
+		encoded[0] =(byte) sizeOfMsg;
 		
+		for (int i = 0; i < payload.length; i++) {
+			encoded[i + 1] = payload[i];		}
+		return encoded;
+		/*
 		if (true)
 		   throw new UnsupportedOperationException(TODO.method());
+		*/
 
-		return encoded;
 		
 	}
 
@@ -40,8 +47,14 @@ public class Message {
 		// TODO
 		// decapsulate the data contained in the received byte array and store it 
 		// in the payload of this message
-		
+		int sizeOfMsg = received[0];
+		byte[] tempArray = new byte[sizeOfMsg];
+		for (int i = 0; i < sizeOfMsg; i++) {
+			tempArray[i] = received[i + 1];
+		}
+		this.payload = tempArray;
+		/*
 		throw new UnsupportedOperationException(TODO.method());
-		
+		*/
 	}
 }
